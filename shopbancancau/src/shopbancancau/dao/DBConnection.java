48af546901a4aef7f3dea1fe4125e243c67fbb57
Thai22TIN01 <ndt22tin01@gmail.com>
2026-01-30 20:48:43 +0700

    phan quyen xem thong ke

--- shopbancancau/src/shopbancancau/dao/DBConnection.java
+++ shopbancancau/src/shopbancancau/dao/DBConnection.java
@@ -8,7 +8,8 @@ public class DBConnection {
     private static final String URL =
         "jdbc:mysql://localhost:3306/shopbancancau?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASSWORD = ""; // XAMPP mặc định để trống
+    private static final String PASSWORD = "123456";
+    //private static final String PASSWORD = ""; Vinh
 
     public static Connection getConnection() {
         try {
